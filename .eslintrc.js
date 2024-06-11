15407e8fae1f0bfcd14e66de1ba76bc74ac5913d
Rodrigo <rodrigo.barriuso@gmail.com>
2024-06-12 01:32:47 +0200

    repo

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   ignorePatterns: ["apps/**", "packages/**"],
-  extends: ["@repo/eslint-config/library.js"],
+  extends: [ ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
